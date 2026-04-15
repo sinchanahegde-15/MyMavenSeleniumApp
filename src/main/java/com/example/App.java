ee623de0800e533adb8fce6ded11d25e57e13be0
sinchanahegde-15 <sinchanachandrashekar15@gmail.com>
2026-04-15 20:28:43 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,31 +6,48 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class App 
-{
-    public static void main(String[] args)
-    {
-        // Setup driver automatically
+public class App {
+    public static void main(String[] args) {
+
+        // Setup ChromeDriver automatically
         WebDriverManager.chromedriver().setup();
 
+        // Configure Chrome options
         ChromeOptions options = new ChromeOptions();
 
-
+        // 🔥 Required for Linux / VirtualBox
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
 
-        // Optional (use if GUI issues)
-        // options.addArguments("--headless=new");
+        // 🔥 MUST for VM (fixes Chrome crash)
+        options.addArguments("--headless=new");
 
+        // Optional safety
+        options.addArguments("--remote-allow-origins=*");
+
+        // Launch browser
         WebDriver driver = new ChromeDriver(options);
 
-        driver.get("https://www.saucedemo.com/");
-        driver.manage().window().maximize();
+        try {
+            // Open website
+            driver.get("https://www.saucedemo.com/");
 
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
+            // Perform actions
+            driver.findElement(By.id("user-name")).sendKeys("standard_user");
+            driver.findElement(By.id("password")).sendKeys("secret_sauce");
+            driver.findElement(By.id("login-button")).click();
 
-        System.out.println("Login successful!");
+            // Print page title
+            System.out.println("Title: " + driver.getTitle());
+
+            // Success message
+            System.out.println("Login successful!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Close browser
+            driver.quit();
+        }
     }
 }
